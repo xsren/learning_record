926360e8c1a5cbda13ce615bb86c0486d1287d5b
xsren <bestrenxs@gmail.com>
2017-02-27 17:46:54 +0800

    add frontera 介绍

---
+++ frontera 介绍/distribute_spider/frontier/spider_settings.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from frontera.settings.default_settings import MIDDLEWARES
+
+MAX_NEXT_REQUESTS = 256
+DELAY_ON_EMPTY = 5.0
+
+MIDDLEWARES.extend([
+    'frontera.contrib.middlewares.domain.DomainMiddleware',
+    'frontera.contrib.middlewares.fingerprint.DomainFingerprintMiddleware'
+])
+
+#--------------------------------------------------------
+# Crawl frontier backend
+#--------------------------------------------------------
+BACKEND = 'frontera.contrib.backends.remote.messagebus.MessageBusBackend'
+SPIDER_FEED_PARTITIONS = 2
+
+
+# MESSAGE_BUS_CODEC='frontera.contrib.backends.remote.codecs.json'
---
+++ frontera 介绍/single_scrapy_spider/tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,22 @@
+import scrapy
+import pdb
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.css('span small::text').extract_first(),
+                'tags': quote.css('div.tags a.tag::text').extract(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            # pdb.set_trace()
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
